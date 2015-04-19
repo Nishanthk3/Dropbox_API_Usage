138cf35f634b6a91fee6be9f28e8cc4ec6c02a5e
Nishanthk3 <nishanth.k3@gmail.com>
2015-04-19 10:07:50 -0500

    Formatted the code

--- dropboxapi/src/main/java/com/nishanth/dropbox/DropboxRestController.java
+++ dropboxapi/src/main/java/com/nishanth/dropbox/DropboxRestController.java
@@ -21,66 +21,66 @@ import org.codehaus.jackson.map.ObjectMapper;
 
 @Controller
 public class DropboxRestController {
-	
+
 	private static final String Dropbox_URL = "https://www.dropbox.com/1/oauth2/authorize";
 	private static final String AccessToken_URL = "https://api.dropbox.com/1/oauth2/token";
 	private static final String Response_Type = "code";
 
 	@Value("${dropbox.APP_KEY}")
 	private String APP_KEY;
-	
+
 	@Value("${dropbox.APP_SECRET}")
 	private String APP_SECRET;
-	
+
 	@Value("${dropbox.REDIRECT_URI}")
 	private String REDIRECT_URI;
-	
+
 	@Value("${dropbox.ENCODE_VALUE}")
 	private String ENCODE_VALUE;
-	
+
 	@RequestMapping( value="/", method = RequestMethod.GET)
 	public @ResponseBody String intialPage()
 	{
 		return "Welcome to Dropbox API";
 	}
-	
+
 	@RequestMapping( value="/dropbox")
 	public @ResponseBody String oAuth()
 	{
-        byte[] byteEncodeValue = Base64.encodeBase64(ENCODE_VALUE.getBytes());
-        String encodeValue = new String(byteEncodeValue);
-        
-        String url = Dropbox_URL+"?client_id="+APP_KEY+"&response_type="+Response_Type+"&redirect_uri="+REDIRECT_URI+"&state="+encodeValue;
-        
-        if(Desktop.isDesktopSupported()){
-            Desktop desktop = Desktop.getDesktop();
-            try {
-                desktop.browse(new URI(url));
-            } catch (Exception e) {
-                e.printStackTrace();
-                throw new RuntimeException();
-            }
-        }else{
-            Runtime runtime = Runtime.getRuntime();
-            try {
-                runtime.exec("xdg-open " + url);
-            } catch (Exception e) {
-                e.printStackTrace();
-                throw new RuntimeException();
-            }
-        }
+		byte[] byteEncodeValue = Base64.encodeBase64(ENCODE_VALUE.getBytes());
+		String encodeValue = new String(byteEncodeValue);
+
+		String url = Dropbox_URL+"?client_id="+APP_KEY+"&response_type="+Response_Type+"&redirect_uri="+REDIRECT_URI+"&state="+encodeValue;
+
+		if(Desktop.isDesktopSupported()){
+			Desktop desktop = Desktop.getDesktop();
+			try {
+				desktop.browse(new URI(url));
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw new RuntimeException();
+			}
+		}else{
+			Runtime runtime = Runtime.getRuntime();
+			try {
+				runtime.exec("xdg-open " + url);
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw new RuntimeException();
+			}
+		}
 		return "redirecting to /mycallback";
 	}
-	
+
 	@RequestMapping( value="/mycallback",method = RequestMethod.GET)
 	public @ResponseBody String oAuthCallback( @RequestParam("code") String code, @RequestParam("state") String state, HttpServletRequest httpReq)
 	{
 		try {
-			 
+
 			Client client = Client.create();
-	 
+
 			WebResource webResource = client.resource(AccessToken_URL);
-	 
+
 			String input = "code="+code+"&"
 					+ "grant_type=authorization_code&"
 					+ "client_id="+APP_KEY+"&"
@@ -89,7 +89,7 @@ public class DropboxRestController {
 
 			ClientResponse response = webResource.type("application/x-www-form-urlencoded")
 					.post(ClientResponse.class, input);
-	 
+
 			if (response.getStatus() != 200) {
 				System.out.println("Response = "+response.getStatus());
 				System.out.println("Content  = "+response.getEntity(String.class));
@@ -97,11 +97,11 @@ public class DropboxRestController {
 			}
 			String str = response.getEntity(String.class);
 			AccessTokenClass accessTokenClass = new ObjectMapper().readValue(str, AccessTokenClass.class);
-		  } catch (Exception e) {
-	 
+		} catch (Exception e) {
+
 			e.printStackTrace();
 			throw new RuntimeException();
-		  }
+		}
 		return "Code Retrieved";
 	}
 
